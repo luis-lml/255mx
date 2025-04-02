6aad1e83ab4ccd430747e971808053600c7c65a8
Luis M.Marmolejo <l.mrmlj@gmail.com>
2025-04-02 16:29:39 -0600

    Adding two files: -config.properties -PropertiesFiles.java
    Moved dosCincoCinco.java into the same package (main) in order to import it to the same folder as PropertiesFiles and config.properties

--- src/main/java/Test/dosCincoCinco.java
+++ src/main/java/Test/dosCincoCinco.java
@@ -1,18 +1,23 @@
+package Test;
+
+import config.PropertiesFile;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.dynalink.beans.StaticClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+
+
 
 public class dosCincoCinco {
-    static String browser;
+    public static String browser;
     static WebDriver driver;
 
     public static void main(String[] args) {
         setBrowser();
+        PropertiesFile.readPropertiesFile();
         setBrowserConfig();
         runTest();
+        PropertiesFile.writePropertiesFile();
         }
 
     public static void runTest() {
---
+++ src/main/java/config/PropertiesFile.java
@@ -0,0 +1,47 @@
+package config;
+
+import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import Test.dosCincoCinco;
+
+
+public class PropertiesFile {
+
+    static Properties properties = new Properties();
+
+    public static void main(String[] args) {
+
+        readPropertiesFile();
+        writePropertiesFile();
+//        readPropertiesFile();
+    }
+
+    public static void readPropertiesFile() {
+        Properties properties = new Properties();
+        try {
+            InputStream input = new FileInputStream("/Users/255_Studio/Documents/Dev-Projects/255mx/src/main/java/config/config.properties");
+            properties.load(input);
+            properties.getProperty("browser");
+            System.out.println(properties.getProperty("browser"));
+
+
+            dosCincoCinco.browser = properties.getProperty("browser");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writePropertiesFile() {
+        try {
+            OutputStream output = new FileOutputStream("/Users/255_Studio/Documents/Dev-Projects/255mx/src/main/java/config/config.properties");
+            properties.setProperty("browser", "Chrome");
+            properties.setProperty("result", "pass");
+            properties.store(output, null);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
