b81f56dc64a6e61a9b0cdb435383d418f7173b01
Luis M.Marmolejo <l.mrmlj@gmail.com>
2025-05-02 16:38:14 -0600

    Adding two files: -config.properties -PropertiesFiles.java -created first retrato click to web page
    Moved dosCincoCinco.java into the same package (main) in order to import it to the same folder as PropertiesFiles and config.properties

--- src/main/java/Test/dosCincoCinco.java
+++ src/main/java/Test/dosCincoCinco.java
@@ -1,27 +1,38 @@
 package Test;
-
 import config.PropertiesFile;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 
 public class dosCincoCinco {
     public static String browser;
     static WebDriver driver;
 
-    public static void main(String[] args) {
-        setBrowser();
+    public static void main(String[] args) throws InterruptedException {
+        //setBrowser();
         PropertiesFile.readPropertiesFile();
         setBrowserConfig();
         runTest();
         PropertiesFile.writePropertiesFile();
         }
 
-    public static void runTest() {
+    public static void runTest() throws InterruptedException {
         driver.get("https://www.255.com.mx/");
+        driver.manage().window().maximize();
+//        driver.getTitle();
+//        System.out.println("Title: "+ Title);
+        Thread.sleep(2000);
+        WebElement buttonRetrato = new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//a[contains(text(), 'Retrato') or contains(@href, 'retrato')]")));
+        buttonRetrato.click();
+        Thread.sleep(2000);
         driver.close();
         driver.quit();
     }
@@ -40,7 +51,5 @@ public class dosCincoCinco {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         }
-
-
     }
 }
